310e6ab5c7009c036109ad7e78b4a638080b243d
Andrew Sokolov <56156851+dushasokol-tasks@users.noreply.github.com>
2021-09-29 10:11:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "s3-webapp" {
   #source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
-  source  = "app.terraform.io/h-test/s3-webapp/aws"
+  source  = "app.terraform.io/example-org-e5af97/s3-webapp/aws"
 
   name        = var.name
   region = var.region
